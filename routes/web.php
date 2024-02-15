80fba92b05ae1b881c433b9b608967ad7a2f86f7
Mfathy222 <mohammed.fathy222@gmail.com>
2024-02-15 23:56:35 +0200

    multi guard auth

--- routes/web.php
+++ routes/web.php
@@ -39,5 +39,5 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+// require __DIR__.'/auth.php';
 require __DIR__.'/dashboard.php';
